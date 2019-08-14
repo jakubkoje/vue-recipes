2b5316cecb444fef08ce85dc3ed3d78351cd7204
jakubkoje <you@example.com>
2019-08-14 14:58:21 +0200

    created working addrecipe functionality

--- src/views/AddRecipe.vue
+++ src/views/AddRecipe.vue
@@ -1,20 +1,45 @@
 <template>
-  <div>
+  <ion-content>
     <Header />
-    <ion-item class="ion-margin">
-      <ion-label position="stacked">Recipe name</ion-label>
-      <ion-input class="background" placeholder="Enter Input"></ion-input>
-    </ion-item>
+    <form @submit="addRecipe">
+      <ion-item class="ion-margin">
+        <ion-label position="stacked">Recipe name</ion-label>
+        <ion-input
+          class="background"
+          placeholder="Enter Input"
+          :value="recipe.name"
+          @ionInput="recipe.name = $event.target.value"
+        ></ion-input>
+      </ion-item>
 
-    <ion-item class="ion-margin">
-      <ion-label position="stacked">Description</ion-label>
-      <ion-textarea
-        rows="4"
-        class="background"
-        placeholder="Write instructions for your recipe here"
-      ></ion-textarea>
-    </ion-item>
-  </div>
+      <ion-item class="ion-margin">
+        <ion-label position="stacked">Description</ion-label>
+        <ion-textarea
+          rows="4"
+          class="background"
+          placeholder="Write instructions for your recipe here"
+          :value="recipe.description"
+          @ionInput="recipe.description = $event.target.value"
+        ></ion-textarea>
+      </ion-item>
+
+      <ion-list class="ion-margin">
+        <ion-item v-for="ingredient in recipe.ingredients" :key="ingredient">
+          <ion-label>{{ingredient}}</ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-input
+            class="background"
+            placeholder="enter ingredient"
+            :value="ingredient"
+            @ionInput="ingredient = $event.target.value"
+          ></ion-input>
+          <ion-button v-on:click="addIngredient()">Click me</ion-button>
+        </ion-item>
+      </ion-list>
+      <ion-button expand="block" type="submit">Submit recipe</ion-button>
+    </form>
+  </ion-content>
 </template>
 
 <script>
@@ -23,6 +48,26 @@ export default {
   name: "addrecipe",
   components: {
     Header
+  },
+  data() {
+    return {
+      recipe: {
+        name: "",
+        ingredients: ["jablko", "rajcina"],
+        description: ""
+      },
+      ingredient: ""
+    };
+  },
+  methods: {
+    addIngredient() {
+      this.recipe.ingredients = [...this.recipe.ingredients, this.ingredient];
+      this.ingredient = "";
+    },
+    addRecipe(e) {
+      e.preventDefault();
+      this.$emit("add-recipe", this.recipe);
+    }
   }
 };
 </script>
