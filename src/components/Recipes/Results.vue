99e6b512e385a45b184670048951459958e13e53
jakubkoje <you@example.com>
2019-08-15 23:11:16 +0200

    nothing important

--- src/components/Recipes/Results.vue
+++ src/components/Recipes/Results.vue
@@ -1,19 +1,19 @@
 <template>
-      <ion-content>
-          <ion-list>
-          <ion-item class="ion-margin">
-            <ion-label position="stacked">Write in your available ingredients</ion-label>
-            <ion-input class="background" placeholder="dfsf"></ion-input>
-            <ion-button size="small" shape="round">+</ion-button>
-          </ion-item>
-          </ion-list>
-      </ion-content>
+  <ion-content>
+    <ion-list>
+      <ion-item class="ion-margin">
+        <ion-label position="stacked">Write in your available ingredients</ion-label>
+        <ion-input class="background" placeholder="dfsf"></ion-input>
+        <ion-button slot="end" shape="round" size="small">+</ion-button>
+      </ion-item>
+    </ion-list>
+  </ion-content>
 </template>
 
 <script>
 export default {
-    name: "Results"
-}
+  name: "Results"
+};
 </script>
 
 <style scoped>
